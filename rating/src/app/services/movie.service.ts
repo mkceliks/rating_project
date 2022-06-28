50cc5157e42531e3130a50b37aa278dd90d0d160
mkceliks <66789068+mkceliks@users.noreply.github.com>
2022-06-28 06:02:09 +0300

    services updated for sports and animes categories.

--- rating/src/app/services/movie.service.ts
+++ rating/src/app/services/movie.service.ts
@@ -2,19 +2,39 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Movie } from '../models/movie';
+import { Anime } from '../models/anime';
+import { Sport } from '../models/sport';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
 
-  apiUrl = 'http://localhost:8000/movies'
+  apiUrl = 'http://localhost:8000/'
 
   constructor(private httpClient: HttpClient) { }
 
   getMovies():Observable<Movie[]> {
 
-    return this.httpClient.get<Movie[]>(this.apiUrl);
+    let newPath = this.apiUrl + "movies"
+
+    return this.httpClient.get<Movie[]>(newPath);
+
+  }
+  
+  getAnimes():Observable<Anime[]> {
+
+    let newPath = this.apiUrl + "animes"
+
+    return this.httpClient.get<Anime[]>(newPath);
+
+  }
+
+  getSports():Observable<Sport[]> {
+
+    let newPath = this.apiUrl + "sports"
+
+    return this.httpClient.get<Sport[]>(newPath);
 
   }
 
--- rating/src/app/services/product-add.service.ts
+++ rating/src/app/services/product-add.service.ts
@@ -2,18 +2,38 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Movie } from '../models/movie';
+import { Anime } from '../models/anime';
+import { Sport } from '../models/sport';
 @Injectable({
   providedIn: 'root'
 })
 export class ProductAddService {
 
-  apiUrl = 'http://localhost:8000/add-anime'
+  apiUrl = 'http://localhost:8000/'
 
   constructor(private httpClient: HttpClient) { }
 
-  add(product:Movie){
+  addMovie(product:Movie){
 
-    return this.httpClient.post(this.apiUrl,product)
+    let newPath = this.apiUrl + "add-movie"
+
+    return this.httpClient.post(newPath,product)
+
+  }
+
+  addAnime(product:Anime){
+
+    let newPath = this.apiUrl + "add-anime"
+
+    return this.httpClient.post(newPath,product)
+
+  }
+
+  addSport(product:Sport){
+
+    let newPath = this.apiUrl + "add-sport"
+
+    return this.httpClient.post(newPath,product)
 
   }
 
