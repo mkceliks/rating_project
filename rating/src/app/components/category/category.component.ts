326051304fe48ce93333d89334662aa789c6078b
mkceliks <66789068+mkceliks@users.noreply.github.com>
2022-06-28 06:05:07 +0300

    navi,dashboard and category components updated.

--- rating/src/app/components/category/category.component.ts
+++ rating/src/app/components/category/category.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Anime } from 'src/app/models/anime';
 import { Movie } from 'src/app/models/movie';
+import { Sport } from 'src/app/models/sport';
 import { MovieService } from 'src/app/services/movie.service';
 
 @Component({
@@ -10,17 +12,33 @@ import { MovieService } from 'src/app/services/movie.service';
 export class CategoryComponent implements OnInit {
 
   movies : Movie[] = [];
+  animes : Anime[] = [];
+  sports : Sport[] = [];
 
   constructor(private movieService:MovieService) { }
 
   ngOnInit(): void {
     this.getMovies();
+    this.getAnimes();
+    this.getSports();
   }
 
 
   getMovies(){
     this.movieService.getMovies().subscribe(response => {
       this.movies = response
+    })
+  }
+
+  getAnimes(){
+    this.movieService.getAnimes().subscribe(response => {
+      this.animes = response
+    })
+  }
+
+  getSports(){
+    this.movieService.getSports().subscribe(response => {
+      this.sports = response
     })
   }
 
--- rating/src/app/components/dashboard/dashboard.component.ts
+++ rating/src/app/components/dashboard/dashboard.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Movie } from 'src/app/models/movie';
 import { MovieService } from 'src/app/services/movie.service';
+import { Anime } from 'src/app/models/anime';
+import { Sport } from 'src/app/models/sport';
 
 @Component({
   selector: 'app-dashboard',
@@ -10,17 +12,34 @@ import { MovieService } from 'src/app/services/movie.service';
 export class DashboardComponent implements OnInit {
 
   movies : Movie[] = [];
+  animes : Anime[] = [];
+  sports : Sport[] = [];
 
   constructor(private movieService:MovieService) { }
 
   ngOnInit(): void {
     this.getMovies();
+    this.getAnimes();
+    this.getSports();
   }
+
 
 
   getMovies(){
     this.movieService.getMovies().subscribe(response => {
       this.movies = response
+    })
+  }
+
+  getAnimes(){
+    this.movieService.getAnimes().subscribe(response => {
+      this.animes = response
+    })
+  }
+
+  getSports(){
+    this.movieService.getSports().subscribe(response => {
+      this.sports = response
     })
   }
 
